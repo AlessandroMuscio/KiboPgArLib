43363a0adcd521423db8e7b51eba5dfda396a608
AlessandroMuscio <56693783+AlessandroMuscio@users.noreply.github.com>
2021-04-20 11:23:07 +0200

    Corretto FormuleNote.java

--- src/it/unibs/fp/mylib/FormuleNote.java
+++ src/it/unibs/fp/mylib/FormuleNote.java
@@ -2,7 +2,7 @@ package it.unibs.fp.mylib;
 
 public class FormuleNote {
 
-  public static int MCM(int a, int b) {
+  public static int MCD(int a, int b) {
     while (a != 0 && b != 0) {
       if (a > b)
         a %= b;
@@ -11,24 +11,6 @@ public class FormuleNote {
     }
 
     return a == 0 ? b : a;
-  }
-
-  public static int MCM(int[] valori) {
-    if (valori == null || valori.length == 0)
-      return -1;
-
-    int mcm = valori[0];
-
-    for (int i = 1; i < valori.length; i++)
-      mcm = MCM(mcm, valori[i]);
-
-    return mcm;
-  }
-
-  public static int MCD(int a, int b) {
-    int mcm = MCM(a, b);
-
-    return (a * b) / mcm;
   }
 
   public static int MCD(int[] valori) {
@@ -41,5 +23,23 @@ public class FormuleNote {
       mcd = MCD(mcd, valori[i]);
 
     return mcd;
+  }
+
+  public static int MCM(int a, int b) {
+    int mcd = MCD(a, b);
+
+    return (a * b) / mcd;
+  }
+
+  public static int MCM(int[] valori) {
+    if (valori == null || valori.length == 0)
+      return -1;
+
+    int mcm = valori[0];
+
+    for (int i = 1; i < valori.length; i++)
+      mcm = MCM(mcm, valori[i]);
+
+    return mcm;
   }
 }
