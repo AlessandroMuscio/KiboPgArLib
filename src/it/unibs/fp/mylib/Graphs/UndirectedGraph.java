d9096f1fbc963a44a69f6e8955c1f7e55f950f0e
AlessandroMuscio <muscioalex30@gmail.com>
2021-05-31 12:40:52 +0200

    Added Default node in graphs

--- src/it/unibs/fp/mylib/Graphs/UndirectedGraph.java
+++ src/it/unibs/fp/mylib/Graphs/UndirectedGraph.java
@@ -1,8 +1,9 @@
 package it.unibs.fp.mylib.Graphs;
 
+import java.util.HashSet;
 import java.util.Set;
 
-public class UndirectedGraph<N, V> implements Graph<N, V> {
+public class UndirectedGraph<N extends DefaultNode, V etends DefaultEdge> implements Graph<N, V> {
   private Set<N> nodes;
   private Set<V> edges;
 
@@ -12,6 +13,47 @@ public class UndirectedGraph<N, V> implements Graph<N, V> {
   }
 
   public UndirectedGraph() {
-    nodes = new 
+    nodes = new HashSet<>();
+    edges = new HashSet<>();
+  }
+
+  @Override
+  public Set<N> getAllNodes() {
+    return nodes;
+  }
+
+  @Override
+  public boolean containsNode(N node) {
+    return nodes.contains(node);
+  }
+
+  @Override
+  public boolean containsEdge(V edge) {
+    return edges.contains(edge);
+  }
+
+  @Override
+  public Set<V> getAllEdges() {
+    return edges;
+  }
+
+  @Override
+  public boolean addNode(N node) {
+    return nodes.add(node);
+  }
+
+  @Override
+  public boolean removeNode(N node) {
+    return nodes.remove(node);
+  }
+
+  @Override
+  public boolean addEdge(V edge) {
+    return edges.add(edge);
+  }
+
+  @Override
+  public boolean removeEdge(V edge) {
+    return edges.remove(edge);
   }
 }
