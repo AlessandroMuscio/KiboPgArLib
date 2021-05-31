33e6b44a80a2ce07adf2fd7667f235ed76f73e5d
AlessandroMuscio <56693783+AlessandroMuscio@users.noreply.github.com>
2021-05-31 18:05:44 +0200

    Created Graphs Branch

---
+++ src/it/unibs/fp/mylib/Graphs/DefaultNode.java
@@ -0,0 +1,34 @@
+public class DefaultNode {
+  private int id;
+
+  private Map<DefaultNode, Double>
+
+  public DefaultNode(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof DefaultNode) {
+      DefaultNode node_obj = (DefaultNode) obj;
+
+      if (id == node_obj.id)
+        return true;
+    }
+
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Node: [Id: %d]", id);
+  }
+}
