ab13901fe0fd5334f18ed73c8713b5899885665f
AlessandroMuscio <56693783+AlessandroMuscio@users.noreply.github.com>
2021-05-31 09:56:03 +0200

    Inizio grafi

---
+++ src/it/unibs/fp/mylib/Graphs/UndirectedGraph.java
@@ -0,0 +1,17 @@
+package it.unibs.fp.mylib.Graphs;
+
+import java.util.Set;
+
+public class UndirectedGraph<N, V> implements Graph<N, V> {
+  private Set<N> nodes;
+  private Set<V> edges;
+
+  public UndirectedGraph(Set<N> nodes, Set<V> edges) {
+    this.nodes = nodes;
+    this.edges = edges;
+  }
+
+  public UndirectedGraph() {
+    nodes = new 
+  }
+}
